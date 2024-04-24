3dd3c0aeec928bb193973d83e31c2f43e88d72a1
ryeong0418 <ryeong2105@gmail.com>
2024-04-24 15:16:28 +0900

    dags_python_operator modify

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -7,7 +7,7 @@ import random
 with DAG(
     dag_id= "dags_python_operator",
     schedule= "30 6 * * *",
-    start_date= pendulum.datetime(2023,3,1,tx="Asia/Seoul"),
+    start_date= pendulum.datetime(2023,3,1,tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     def select_fruit():
