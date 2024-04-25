276d36b924c17cb2a11a9f39ae92901732e1eeb2
ryeong0418 <ryeong2105@gmail.com>
2024-04-25 10:40:05 +0900

    args use

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -4,19 +4,19 @@ import datetime
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id = "dags_bah_select_fruit",
-    schedule = "10 0 * * 6#1", #매월 첫 번째 주 토요일 0시10분
-    start_date = pendulum.datetime(2023,3,1,tz="Asia/Seoul"),
-    catchup = False
+    dag_id="dags_bash_select_fruit",
+    schedule="10 0 * * 6#1", #매월 첫 번째 주 토요일 0시10분
+    start_date=pendulum.datetime(2023,3,1,tz="Asia/Seoul"),
+    catchup=False
 ) as dag:
 
     t1_orange = BashOperator(
-        task_id = "t1_orange",
+        task_id="t1_orange",
         bash_command="/opt/airflow/plugins/shell/select_fruits.sh ORANGE",
     )
 
     t2_avocado = BashOperator(
-        task_id = "t2_avocado",
+        task_id="t2_avocado",
         bash_command="/opt/airflow/plugins/shell/select_fruits.sh AVOCADO",
     )
 
