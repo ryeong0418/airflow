2974951ba125c3f1883a68be2dea6a84656390b2
ryeong0418 <ryeong2105@gmail.com>
2024-04-26 17:14:19 +0900

    macro code

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_with_template",
     schedule="10 0 * * *",
-    start_date=pendulum.datetime(2023,3,1,tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024,4,1,tz="Asia/Seoul"),
     catchup=False
 ) as dag:
 
