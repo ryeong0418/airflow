98091f18ae3925ef57ab27f30b7f8bd2641769e9
ryeong0418 <ryeong2105@gmail.com>
2024-05-14 13:54:11 +0900

    api operator upload

---
+++ dags/dags_seoul_api_corona.py
@@ -0,0 +1,28 @@
+from operators.seoul_api_to_csv_operator import SeoulApiToCsvOperator
+from airflow import DAG
+import pendulum
+
+with DAG(
+    dag_id='dags_seoul_api_corona',
+    schedule='0 7 * * *',
+    start_date=pendulum.datetime(2023,3,1,tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    '''서울시 코로나 19 확진자 발생 동향'''
+
+    tb_corona19_count_status=SeoulApiToCsvOperator(
+        task_id='tb_corona19_count_status',
+        dataset_nm='TbCorona19CountStatus',
+        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul")|ds_nodash}}',
+        file_name='TbCorona19CountStatus.csv'
+    )
+
+    '''서울시 코로나19 백신 예방 접종 현황'''
+    tv_corona19_vaccine_stat_new=SeoulApiToCsvOperator(
+        task_id='tv_corona19_vaccine_stat_new',
+        dataset_nm='TvCorona19CountStatus',
+        path='/opt/airflow/files/TvCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul")|ds_nodash}}',
+        file_name='TvCorona19CountStatus.csv'
+    )
+
+    tb_corona19_count_status >> tv_corona19_vaccine_stat_new
