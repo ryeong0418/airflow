592a0206824f8c78979d96063a7a2a348057e192
ryeong0418 <ryeong2105@gmail.com>
2024-07-05 12:52:04 +0900

    nx 수정

--- dags/dags_extract_nx.py
+++ dags/dags_extract_nx.py
@@ -7,16 +7,15 @@ import pendulum
 import os
 
 
-def generate_url(execution_date):
-
-    created_url = f"{os.getenv('MAP_URI')}?date={execution_date.strftime('%Y-%m-%d')}"
-    print(created_url)
-
-    return created_url
-
+# def generate_url(execution_date):
+#
+#     created_url = f"{os.getenv('MAP_URI')}?date={execution_date.strftime('%Y-%m-%d')}"
+#     print(created_url)
+#
+#     return created_url
 
 with DAG(
-    dag_id='dag_extract_nx',
+    dag_id='dags_extract_nx',
     start_date=pendulum.datetime(2024,5,1,tz='Asia/Seoul'), #start_date는 해당 날짜 포함하지만
     end_date=pendulum.datetime(2024,6,1,tz='Asia/Seoul'), #end_date는 해당 날짜 포함 안 함
     catchup=False,
@@ -24,23 +23,23 @@ with DAG(
 ) as dag:
 
     # URL 생성 작업을 PythonOperator로 정의
-    def generate_url_task(**kwargs):
-        ti = kwargs['ti']
-        execution_date = kwargs['execution_date']
-        url = generate_url(execution_date)
-        ti.xcom_push(key='url', value=url)
-
-    generate_url_operator = PythonOperator(
-        task_id='generate_url_task',
-        python_callable=generate_url_task,
-        provide_context=True,
-        dag=dag,
-    )
+    # def generate_url_task(**kwargs):
+    #     ti = kwargs['ti']
+    #     execution_date = kwargs['execution_date']
+    #     url = generate_url(execution_date)
+    #     ti.xcom_push(key='url', value=url)
+    #
+    # generate_url_operator = PythonOperator(
+    #     task_id='generate_url_task',
+    #     python_callable=generate_url_task,
+    #     provide_context=True,
+    #     dag=dag,
+    # )
 
     nx_info = SimpleHttpOperator(
         task_id='nx',
         http_conn_id='nx_api',
-        endpoint="{{task_instance.xcom_pull(task_ids='generate_url_task', key='url') }}",
+        endpoint='https://open.api.nexon.com/maplestory/v1/ranking/overall?date=2024-05-01',
         method='GET',
         headers={'x-nxopen-api-key': os.getenv('API_KEY'),
         }
@@ -55,4 +54,4 @@ with DAG(
 
         pprint(json.loads(rslt))
 
-    generate_url_operator >> nx_info >> python_2()
+    nx_info >> python_2()
