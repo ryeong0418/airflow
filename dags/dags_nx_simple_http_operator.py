549edc8e53398b4aba07728319d699b8c90b6305
ryeong0418 <ryeong2105@gmail.com>
2024-07-08 15:36:42 +0900

    nx_blob_add

--- dags/dags_nx_simple_http_operator.py
+++ dags/dags_nx_simple_http_operator.py
@@ -1,19 +1,12 @@
 from airflow import DAG
 from operators.nx_api_to_json_operator import NxApiToJsonOperator
 from airflow.operators.python import PythonOperator
+from azure.storage.blob import BlobServiceClient
+from airflow.decorators import task
 import pendulum
 import os
 import json
-
-
-def save_json_locally(**kwargs):
-    ti = kwargs['ti']
-    raw_data = ti.xcom_pull(task_ids='nx_info')
-    file_path = 'C:/Users/seoryeong/Desktop/folder/nx_code.json'
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(raw_data, f, ensure_ascii=False, indent=4)
-    print(f"JSON 데이터가 '{file_path}' 파일에 저장되었습니다.")
+from datetime import datetime
 
 
 with DAG(
@@ -29,13 +22,35 @@ with DAG(
         endpoint='/maplestory/v1/ranking/overall?date=2024-05-01',
     )
 
-    save_to_local = PythonOperator(
-        task_id='save_to_local',
-        python_callable=save_json_locally,
-        provide_context=True,
-    )
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_ids='nx_info')
+        print(rslt)
 
-    nx_info >> save_to_local
+        blob_service_client = BlobServiceClient.from_connection_string('DefaultEndpointsProtocol=https;AccountName=nxpro;AccountKey=DpkKa4WDxfHbD6TbUCyaRDYu1G838cOtDOaaLRZHQ6+a+AGjGOL6grqPpePhvJHT5SRjAvLecsC9+AStRJALvw==;EndpointSuffix=core.windows.net')
+        container_client = blob_service_client.get_container_client('nxcontainer')
+
+        raw_data = rslt
+        today_date = datetime.today()
+        filename = f"nx_extract_data-{today_date}.json"
+
+        try:
+            blob_client = container_client.get_blob_client(filename)
+            data_json = json.dumps(raw_data, indent=4, sort_keys=True, ensure_ascii = False)
+            blob_client.upload_blob(data_json, blob_type="BlockBlob")
+
+            return "Upload successful"
+
+        except Exception as e:
+            return f"An error occurred while uploading to Blob Storage: {str(e)}"
+
+
+
+
+
+
+    nx_info >> python_2()
 
 
 
