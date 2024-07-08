fe282e36e27c53f1c37bf1192443a81fc85aad9a
ryeong0418 <ryeong2105@gmail.com>
2024-07-08 16:29:54 +0900

    nx 로직 변경

--- dags/dags_nx_simple_http_operator.py
+++ dags/dags_nx_simple_http_operator.py
@@ -2,11 +2,33 @@ from airflow import DAG
 from operators.nx_api_to_json_operator import NxApiToJsonOperator
 from azure.storage.blob import BlobServiceClient
 from airflow.decorators import task
+from airflow.operators.python import PythonOperator
 import pendulum
 import json
 from datetime import datetime
 from airflow.models import Variable
 
+def upload_to_blob_storage(date_str, **kwargs):
+    ti = kwargs['ti']
+    rslt = ti.xcom_pull(task_ids=f'nx_info_{date_str}')
+
+    conn_str = Variable.get('connection_string')
+    container_name = Variable.get('container_name')
+    blob_service_client = BlobServiceClient.from_connection_string(conn_str)
+    container_client = blob_service_client.get_container_client(container_name)
+
+    raw_data = rslt
+    filename = f"nx_extract_data-{date_str}.json"
+
+    try:
+        blob_client = container_client.get_blob_client(filename)
+        data_json = json.dumps(raw_data, indent=4, sort_keys=True, ensure_ascii=False)
+        blob_client.upload_blob(data_json, blob_type="BlockBlob")
+
+        return "Upload successful"
+
+    except Exception as e:
+        return f"An error occurred while uploading to Blob Storage: {str(e)}"
 
 with DAG(
     dag_id='dags_nx_simple_http_operator',
@@ -18,45 +40,31 @@ with DAG(
 
     start_date = pendulum.datetime(2024, 5, 1, tz='Asia/Seoul')
     end_date = pendulum.datetime(2024, 6, 1, tz='Asia/Seoul')
+    current_date = start_date
 
-    date = start_date
+    while current_date < end_date:
 
-    while date < end_date:
-        current_date_str = date.format('YYYY-MM-DD')
+        date_str = current_date.format('YYYY-MM-DD')
 
-    nx_info = NxApiToJsonOperator(
-        task_id=f'nx_info_{current_date_str}',
-        http_conn_id='nx_api',
-        endpoint=f'/maplestory/v1/ranking/overall?date={current_date_str}',
-    )
+        nx_info_task = NxApiToJsonOperator(
+            task_id=f'nx_info_{date_str}',
+            http_conn_id='nx_api',
+            endpoint=f'/maplestory/v1/ranking/overall?date={date_str}',
+            dag=dag
+        )
 
-    @task(task_id=f'python_{current_date_str}')
-    def python_task(**kwargs):
-        ti = kwargs['ti']
-        rslt = ti.xcom_pull(task_ids=f'nx_info_{current_date_str}')
+        python_task = PythonOperator(
+            task_id=f'python_task_{date_str}',
+            python_callable=upload_to_blob_storage,
+            op_args=[date_str],
+            provide_context=True,
+            dag=dag
+        )
 
-        conn_str = Variable.get('connection_string')
-        container_name = Variable.get('container_name')
-        blob_service_client = BlobServiceClient.from_connection_string(conn_str)
-        container_client = blob_service_client.get_container_client(container_name)
+        nx_info_task >> python_task
 
-        raw_data = rslt
-        filename = f"nx_extract_data-{current_date_str}.json"
+        current_date = current_date.add(days=1)
 
-        try:
-            blob_client = container_client.get_blob_client(filename)
-            data_json = json.dumps(raw_data, indent=4, sort_keys=True, ensure_ascii = False)
-            blob_client.upload_blob(data_json, blob_type="BlockBlob")
-
-            return "Upload successful"
-
-        except Exception as e:
-            return f"An error occurred while uploading to Blob Storage: {str(e)}"
-
-
-    nx_info >> python_task(current_date_str)
-
-    date = date.add(days=1)
 
 
 
